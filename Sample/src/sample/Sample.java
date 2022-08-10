0fc42106acd6b05d20c15440ae8eef7ad39df257
jidnyasa-11 <jid.falak30@gmail.com>
2022-08-10 16:43:07 +0530

    Commmit by jidnyasa

---
+++ Sample/src/sample/Sample.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is from jidnyasa");
+
+	}
+
+}
